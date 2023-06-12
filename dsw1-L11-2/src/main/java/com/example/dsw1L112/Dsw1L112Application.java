b19c7c75a4c4c892553130b9025fc40400e22f0c
DavidCaldas934 <davidcaldas934@gmail.com>
2023-06-12 09:07:40 -0500

    vamos

---
+++ dsw1-L11-2/src/main/java/com/example/dsw1L112/Dsw1L112Application.java
@@ -0,0 +1,13 @@
+package com.example.dsw1L112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw1L112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsw1L112Application.class, args);
+	}
+
+}
---
+++ dsw1-L11-2/src/test/java/com/example/dsw1L112/Dsw1L112ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsw1L112;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1L112ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
